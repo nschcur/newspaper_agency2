25a3bd994e41cc02a9be2fbff6f9b067e949c32d
nschcur <nazae530@gmail.com>
2024-01-11 23:11:24 +0200

    create test for views

--- topic/tests.py
+++ topic/tests.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 from django.utils import timezone
 
 from .forms import LoginForm, SignUpForm, NewspaperAdminForm, RedactorCreationForm
@@ -97,3 +98,45 @@ class RedactorCreationFormTest(TestCase):
         form = RedactorCreationForm(data=form_data)
         self.assertTrue(form.is_valid())
 
+
+class TestViews(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = get_user_model().objects.create_user(
+            username='testuser',
+            password='testpassword'
+        )
+        self.redactor = Redactor.objects.create(username='test_redactor')
+        self.topic = Topic.objects.create(name='Test Topic')
+        self.newspaper = Newspaper.objects.create(
+            title='Test Newspaper',
+            content='Some test content.',
+            topic=self.topic
+        )
+
+    def test_login_view(self):
+        response = self.client.post(reverse('newspaper_agency:login'), {'username': 'testuser', 'password': 'testpassword'})
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, '/')
+
+    def test_register_user_view(self):
+        response = self.client.post(reverse('newspaper_agency:register'), {'username': 'newuser', 'password1': 'newpassword', 'password2': 'newpassword'})
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('newspaper_agency:register-success'))
+
+    def test_index_view(self):
+        self.client.login(username='testuser', password='testpassword')
+        response = self.client.get(reverse('newspaper_agency:index'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_redactor_list_view(self):
+        response = self.client.get(reverse('newspaper_agency:redactor-list'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_topic_list_view(self):
+        response = self.client.get(reverse('newspaper_agency:topic-list'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_newspaper_list_view(self):
+        response = self.client.get(reverse('newspaper_agency:newspaper-list'))
+        self.assertEqual(response.status_code, 200)
