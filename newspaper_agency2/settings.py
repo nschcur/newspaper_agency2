1e218d50a2278e430339cd967342a7253fbd5f12
nschcur <nazae530@gmail.com>
2024-01-11 22:05:45 +0200

    create models and migrations have been made

--- newspaper_agency2/settings.py
+++ newspaper_agency2/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "topic",
+    'crispy_forms',
+    'crispy_bootstrap5',
+
 ]
 
 MIDDLEWARE = [
@@ -121,4 +125,17 @@ STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "topic.Redactor"
+
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/"
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
+
--- topic/models.py
+++ topic/models.py
@@ -1,3 +1,57 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+from newspaper_agency2 import settings
+
+
+class Redactor(AbstractUser):
+
+    years_of_experience = models.IntegerField(
+        blank=True, null=True,
+        validators=[MaxValueValidator(50), MinValueValidator(1)]
+    )
+
+    class Meta:
+        ordering = ("id", )
+
+    def __str__(self):
+        return f"{self.username}: {self.first_name} {self.last_name}"
+
+
+class Topic(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ("name", )
+
+    def __str__(self):
+        return self.name
+
+
+class Newspaper(models.Model):
+    title = models.CharField(max_length=255, unique=True)
+    content = models.TextField()
+    published_date = models.DateField(auto_now_add=True)
+
+    topic = models.ForeignKey(
+        Topic,
+        on_delete=models.DO_NOTHING,
+        related_name="subjects"
+    )
+
+    publishers = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        related_name="newspaper_redactors",
+    )
+    additional_topics = models.ManyToManyField(
+        Topic,
+        related_name="topics",
+    )
+
+    class Meta:
+        ordering = ("published_date", )
+
+    def __str__(self):
+        return (f"title: {self.title},"
+                f"topic: {self.topic}, published date: {self.published_date}")
