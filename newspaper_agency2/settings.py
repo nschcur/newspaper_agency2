53e1f5da864949be9cf51026b6faa7ad069374e4
nschcur <nazae530@gmail.com>
2024-01-11 22:51:42 +0200

    add secret key

--- newspaper_agency2/settings.py
+++ newspaper_agency2/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -139,3 +139,5 @@ INTERNAL_IPS = [
     "127.0.0.1",
 ]
 
+
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
