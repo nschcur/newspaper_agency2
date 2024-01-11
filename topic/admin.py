1cf84dd80eb86f80d9231d86d204519cdd1d3876
nschcur <nazae530@gmail.com>
2024-01-11 22:10:45 +0200

    create file admin

--- topic/admin.py
+++ topic/admin.py
@@ -1,3 +1,42 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from topic.forms import NewspaperAdminForm
+from topic.models import Topic, Redactor, Newspaper
+
+
+@admin.register(Redactor)
+class RedactorAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("years_of_experience",)
+    fieldsets = UserAdmin.fieldsets + (
+        ("Additional info", {"fields": ("years_of_experience",)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Additional info",
+         {
+             "fields": (
+                 "first_name",
+                 "last_name",
+                 "email",
+                 "years_of_experience",
+             )
+         }
+         ),
+    )
+    list_filter = ["years_of_experience", ]
+    search_fields = ["username", ]
+
+
+@admin.register(Topic)
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ["name", ]
+    list_filter = ["id", ]
+    search_fields = ["name", ]
+
+
+@admin.register(Newspaper)
+class NewspaperAdmin(admin.ModelAdmin):
+    list_display = ("title", "published_date", "topic")
+    list_filter = ["published_date", ]
+    search_fields = ["title", ]
+    form = NewspaperAdminForm
