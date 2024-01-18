53ef87dd33f545068af58ff5ec30f8036a217f04
nschcur <nazae530@gmail.com>
2024-01-18 20:41:00 +0200

    add allowed hosts

--- newspaper_agency2/settings.py
+++ newspaper_agency2/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://newspaper-agency-j00j.onrender.com']
+ALLOWED_HOSTS = ['newspaper-agency-j00j.onrender.com']
 
 
 # Application definition
