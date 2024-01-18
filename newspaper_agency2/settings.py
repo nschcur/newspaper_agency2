af903f10eac2f2d886392548b06ffb2d1141a270
nschcur <nazae530@gmail.com>
2024-01-18 20:35:56 +0200

    Debag true

--- newspaper_agency2/settings.py
+++ newspaper_agency2/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'RENDER' not in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = ['https://newspaper-agency-j00j.onrender.com']
 
