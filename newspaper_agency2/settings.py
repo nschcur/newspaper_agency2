b73ae4b163105fc71611771e09c58bf561009e06
nschcur <nazae530@gmail.com>
2024-01-19 21:41:39 +0200

    add base dir to static

--- newspaper_agency2/settings.py
+++ newspaper_agency2/settings.py
@@ -21,12 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'default_secret_key')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = ['newspaper-agency-j00j.onrender.com']
+ALLOWED_HOSTS = ['127.0.0.1','newspaper-agency-j00j.onrender.com']
 
 
 # Application definition
@@ -127,8 +128,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = "staticfiles/"
+STATIC_URL = "static/"
+
+STATICFILES_DIRS = (BASE_DIR / 'static',)
+
+
+STATIC_ROOT = 'staticfiles/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
